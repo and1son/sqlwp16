cf323c53b5ebeb7e6f998f481fd66937f578e748
Andi Bašić <andi.basic10@gmail.com>
2018-02-20 11:12:02 +0100

    Commit 2

--- EdunovaAPP/konfiguracija.php
+++ EdunovaAPP/konfiguracija.php
@@ -8,3 +8,10 @@ $putanjaAPP = "/EdunovaAPP/";
 $naslovAPP="Edunova APP";
 $appID="EDUNOVAAPP";
 $dev=true;
+
+$host="localhost";
+$dbname="edunovapp16";
+$dbuser="edunova";
+$dbpass="edunova";
+
+$veza = new PDO("mysql:host=" . $host . ";dbname=" . $dbname,$dbuser,$dbpass);
